f3502fbef6fb7ffa116bd68d7d2a6e499d5ace74
Jonathan Avila <jeavila6@miners.utep.edu>
2020-02-11 12:22:54 -0700

    plot ratings for individual dialogs

--- predict/build_dataset.py
+++ predict/build_dataset.py
@@ -1,5 +1,6 @@
 import os
 
+import matplotlib.pyplot as plt
 import numpy as np
 from scipy.io import loadmat
 
@@ -9,9 +10,16 @@ if __name__ == '__main__':
     annotations_dir = r'..\annotations'
     features_dir = r'..\features'
 
+    frame_ms = 20
+    min_rating = 0
+    max_rating = 20
+
     dataset = []
 
-    for file in os.listdir(annotations_dir):
+    fig, axes = plt.subplots(nrows=3, ncols=4)
+    axes = axes.flatten()
+
+    for i, file in enumerate(os.listdir(annotations_dir)):
 
         name = os.path.splitext(file)[0]
 
@@ -25,6 +33,12 @@ if __name__ == '__main__':
         # shorten ratings to match features (there is some delay when stopping the recording)
         ratings = ratings[:features.shape[0]]
 
+        # add plot to figure
+        x_axis = np.linspace(start=0, stop=ratings.shape[0] * frame_ms / 1000, num=ratings.shape[0])
+        axes[i].plot(x_axis, ratings)
+        axes[i].set_title(f'Dialog {i + 1}')
+        axes[i].set_ylim([min_rating, max_rating])
+
         # size is (n, m) where n is the number of samples and m is the number of features plus 1 for ratings
         features_and_ratings = np.concatenate((features, ratings), axis=1)
 
@@ -32,3 +46,5 @@ if __name__ == '__main__':
             dataset.append(sample)
 
     np.save('dataset', np.asarray(dataset))
+
+    plt.show()
