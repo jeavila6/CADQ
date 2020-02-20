b7a29da1fdc1df18854080127291cd49dae7fc91
Jonathan Avila <jeavila6@miners.utep.edu>
2020-02-20 11:53:38 -0700

    pulled out plotting code

--- predict/build_dataset.py
+++ predict/build_dataset.py
@@ -1,43 +1,42 @@
 import os
 
-import matplotlib.pyplot as plt
 import numpy as np
 from scipy.io import loadmat
 
 if __name__ == '__main__':
 
-    # annotation and feature files must have matching names
+    # annotation and feature files must reside in 'annotations' and 'features' directories, respectively,
+    # and must have matching filenames
     annotations_dir = r'..\annotations'
     features_dir = r'..\features'
 
-    frame_ms = 20
+    frame_size_ms = 20
     min_rating = 0
     max_rating = 20
 
     dataset = []
 
-    fig, axes = plt.subplots(nrows=3, ncols=4)
-    axes = axes.flatten()
+    annotation_files = os.listdir(annotations_dir)
 
-    for i, file in enumerate(os.listdir(annotations_dir)):
+    for i, annotation_file in enumerate(annotation_files):
 
-        name = os.path.splitext(file)[0]
+        filename = os.path.splitext(annotation_file)[0]
 
-        features_mat = loadmat(os.path.join(features_dir, name))
+        # load feature matrix with matching filename
+        features_mat = loadmat(os.path.join(features_dir, filename))
         features = features_mat['monster']
 
-        ratings_mat = loadmat(os.path.join(annotations_dir, name))
+        # load annotation matrix with matching filename and extract ratings column
+        ratings_mat = loadmat(os.path.join(annotations_dir, filename))
         ratings = ratings_mat['annotation']['ratings'][0, 0]
         ratings = np.transpose(ratings)
 
-        # shorten ratings to match features (there is some delay when stopping the recording)
-        ratings = ratings[:features.shape[0]]
-
-        # add plot to figure
-        x_axis = np.linspace(start=0, stop=ratings.shape[0] * frame_ms / 1000, num=ratings.shape[0])
-        axes[i].plot(x_axis, ratings)
-        axes[i].set_title(f'Dialog {i + 1}')
-        axes[i].set_ylim([min_rating, max_rating])
+        # shorten features or ratings matrix to match length
+        # the ratings matrix is typically longer because there is some delay when stopping the recording
+        if ratings.shape[0] > features.shape[0]:
+            ratings = ratings[:features.shape[0]]
+        else:
+            features = features[:ratings.shape[0]]
 
         # size is (n, m) where n is the number of samples and m is the number of features plus 1 for ratings
         features_and_ratings = np.concatenate((features, ratings), axis=1)
@@ -46,5 +45,3 @@ if __name__ == '__main__':
             dataset.append(sample)
 
     np.save('dataset', np.asarray(dataset))
-
-    plt.show()
