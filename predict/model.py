2613dcf06a4aee59e2f7f9f09311cb1a094de13f
Jonathan Avila <jonathan.edav@gmail.com>
2020-03-05 11:22:50 -0700

    Update output layer output shape

--- predict/model.py
+++ predict/model.py
@@ -10,40 +10,44 @@ def load_dataset(filename):
     x = dataset[:, :-1]
     y = dataset[:, -1]
 
+    n_features = x.shape[1]
+    n_timesteps = 45
+
     # standardize each feature
     x = scale(x)
 
-    n_timesteps = 45
-    n_features = x.shape[1]
-
-    x = np.reshape(x, (-1, n_timesteps, n_features))
-    y = np.reshape(y, (-1, n_timesteps))
+    # reshape for LSTM
+    x = np.reshape(x, (-1, n_timesteps, n_features))  # x.shape is (n_samples, n_timesteps, n_features)
+    y = y[::n_timesteps]  # y.shape is (n_samples,)
 
     train_size = 0.75
     split_ind = int(x.shape[0] * train_size)
 
     train_x = x[:split_ind, :, :]
     test_x = x[split_ind:, :, :]
-    train_y = y[:split_ind, :]
-    test_y = y[split_ind:, :]
+    train_y = y[:split_ind]
+    test_y = y[split_ind:]
 
     return train_x, test_x, train_y, test_y
 
 
 if __name__ == '__main__':
+    batch_size = 128
+    epochs = 5
+
     x_train, x_test, y_train, y_test = load_dataset('dataset.npy')
 
     model = Sequential()
 
-    model.add(LSTM(units=20, activation='relu', return_sequences=True, dropout=0.2,
+    model.add(LSTM(units=40, activation='relu', return_sequences=True, dropout=0.4,
                    input_shape=(x_train.shape[1], x_train.shape[2])))
-    model.add(LSTM(units=40, activation='relu', dropout=0.4))
-    model.add(Dense(units=45))
+    model.add(LSTM(units=20, activation='relu', dropout=0.5))
+    model.add(Dense(units=1))
 
     model.summary()
 
     model.compile(loss='mse', optimizer='adam', metrics=['mse', 'mae'])
 
-    model.fit(x=x_train, y=y_train, batch_size=128, epochs=5, validation_data=(x_test, y_test))
+    history = model.fit(x=x_train, y=y_train, batch_size=batch_size, epochs=epochs, validation_data=(x_test, y_test))
 
     model.save('saved_model.h5')
